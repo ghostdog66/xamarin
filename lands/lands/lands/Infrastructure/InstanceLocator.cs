f86d247a738e9f1684e0a3a14e25b3e483855adc
Ghostdog66 <ppen_llamas@hotmail.com>
2018-07-23 01:13:49 +0200

    Commit Login Lands v1.1

---
+++ lands/lands/lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace lands.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Xamarin.Forms;
+    using ViewModels;
+
+    public class InstanceLocator : ContentPage
+    {
+        #region Properties
+        
+        public MainViewModel Main { get; set; }
+
+
+        #endregion
+
+
+
+
+        #region Constructors
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+
+
+
+        #endregion
+
+    }
+}
